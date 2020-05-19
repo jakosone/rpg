4461dac0dc232c61d434facdd0aeeca017c1e2fe
juuso <juusoantti@gmail.com>
2020-05-19 16:35:20 +0300

    Inventory functions added.

--- rpg.lisp
+++ rpg.lisp
@@ -1,6 +1,6 @@
-;;;Creating a character list
+;;;Creating a character (list)
 (defun newcharacter (name class)
-  (list name class '0)
+  (list name class '0 (newinventory))
   )
 
 ;;;Getter for character name
@@ -13,12 +13,30 @@
   (second character)
   )
 
-;;;Getter for character XP
+;;;Getter for character XP points
 (defun getxp (character)
   (third character)
   )
 
-;;;Increase XP points
+;;;Getter for character inventory (list)
+(defun getinventory (character)
+  (fourth character)
+  )
+
+;;;Increase XP points of a character
 (defun addxp (character points)
   (setf (third character) (+ (third character) points))
+  )
+
+;;;Creating an inventory
+(defun newinventory ()
+  (list ())
+  )
+
+;;;Adding an item in an inventory of a character
+(defun additem (character item)
+  (if (null (car (getinventory character)))
+      (setf (fourth character) (list item))
+      (setf (fourth character) (push item (fourth character)))
+      )
   )
