61bc6213fa8bc39a5eae04c3b7f1914c6e4a6346
juuso <juusoantti@gmail.com>
2020-05-19 17:18:27 +0300

    Health functions added.

--- rpg.lisp
+++ rpg.lisp
@@ -1,6 +1,6 @@
 ;;;Creating a character (list)
 (defun newcharacter (name class)
-  (list name class '0 (newinventory))
+  (list name class '100 '0 (newinventory))
   )
 
 ;;;Getter for character name
@@ -13,19 +13,40 @@
   (second character)
   )
 
+;;;Getter for character health
+(defun gethealth (character)
+  (third character)
+  )
+
+;;;Present health bar for a character
+(defun healthbar (character)
+  (format t "[")
+  (dotimes (n (/ (gethealth character) 2))
+	   (format t "|"))
+  (format t "] ~D %" (gethealth character))
+  )
+
 ;;;Getter for character XP points
 (defun getxp (character)
-  (third character)
+  (fourth character)
   )
 
 ;;;Getter for character inventory (list)
 (defun getinventory (character)
-  (fourth character)
+  (fifth character)
   )
 
 ;;;Increase XP points of a character
 (defun addxp (character points)
-  (setf (third character) (+ (third character) points))
+  (setf (fourth character) (+ (fourth character) points))
+  )
+
+;;;Get level for a character
+(defun getlevel (character)
+  (cond ((< (getxp character) '7) (return-from getlevel 'Novice))
+	((< (getxp character) '10) (return-from getlevel 'Warrior))
+	(t (return-from getlevel 'Unknown))
+	)
   )
 
 ;;;Creating an inventory
@@ -36,7 +57,21 @@
 ;;;Adding an item in an inventory of a character
 (defun additem (character item)
   (if (null (car (getinventory character)))
-      (setf (fourth character) (list item))
-      (setf (fourth character) (push item (fourth character)))
+      (setf (fifth character) (list item))
+      (setf (fifth character) (push item (fifth character)))
       )
   )
+
+;;;Present character as text
+(defun present (character)
+  (format t "You are ~A, a ~A with ~D XP. "
+	  (getname character) (getclass character) (getxp character))
+  (terpri)
+  (format t "You are carrying: ~A" (getinventory character))
+  (terpri)
+  (terpri)
+  (format t "Health:")
+  (terpri)
+  (healthbar character)
+  )
+
