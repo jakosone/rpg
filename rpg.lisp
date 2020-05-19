2247f0563dd7a4dc6a379f9397e13f0b1262ed36
juuso <juusoantti@gmail.com>
2020-05-19 15:40:11 +0300

    XP functions added.

--- rpg.lisp
+++ rpg.lisp
@@ -1,14 +1,24 @@
-;Creating a character list
+;;;Creating a character list
 (defun newcharacter (name class)
   (list name class '0)
   )
 
-;Getter for character name
+;;;Getter for character name
 (defun getname (character)
   (first character)
   )
 
-;Getter for character class
+;;;Getter for character class
 (defun getclass (character)
   (second character)
+  )
+
+;;;Getter for character XP
+(defun getxp (character)
+  (third character)
+  )
+
+;;;Increase XP points
+(defun addxp (character points)
+  (setf (third character) (+ (third character) points))
   )
