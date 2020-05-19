4dbd17c2bbc60e0a56ecdb0ab889e4764cf2dde5
juuso <juusoantti@gmail.com>
2020-05-19 15:30:58 +0300

    New getter functions.

--- rpg.lisp
+++ rpg.lisp
@@ -1,7 +1,14 @@
+;Creating a character list
 (defun newcharacter (name class)
   (list name class '0)
   )
 
+;Getter for character name
 (defun getname (character)
-  (car character)
+  (first character)
+  )
+
+;Getter for character class
+(defun getclass (character)
+  (second character)
   )
