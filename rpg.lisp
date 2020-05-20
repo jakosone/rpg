6f4f45260fb2275d1e4c2b6817348ca7fac2fe8d
juuso <juusoantti@gmail.com>
2020-05-20 10:49:17 +0300

    A lot of new stuff: mainly map functions.

--- rpg.lisp
+++ rpg.lisp
@@ -1,19 +1,43 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;Global Variables;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;Map lists
 (defvar *mapx* '0)
 (defvar *mapy* '0)
 
+;;;Max values for map size
+(defvar *xmax* 10)
+(defvar *ymax* 10)
+
+;;:List for items in the map
+(defvar *itemlist* '())
+(setf *itemlist* nil)
+
+
+;;;Put items in coordinates
+(defun createitems (count)
+  (dotimes (n count)
+    (setf *itemlist*
+	  (push (list (random 10) (random 10)) *itemlist*))
+    )
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;Map Functions;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;Get map location
-(defun getlocation ()
-  (cond ((pcoordinates 0 0) (return-from getlocation 'Home))
-        ((pcoordinates 0 1) (return-from getlocation 'Woods))
-	((pcoordinates 1 0) (return-from getlocation 'Woods))
-	(t (return-from getlocation 'Unknown))
+(defun coordtoloc (x y)
+  (cond ((peqcoords x y 0 0) (return-from coordtoloc 'Home))
+        ((peqcoords x y 0 1) (return-from coordtoloc 'Woods))
+	((peqcoords x y 1 0) (return-from coordtoloc 'Woods))
+	(t (return-from coordtoloc 'Unknown))
 	)
   )
 	
-;;;Coordinates
-(defun pcoordinates (x y)
-  (and (eql *mapx* x) (eql *mapy* y))
+(defun peqcoords (x y a b)
+  (and (eql x a) (eql y b))
   )
 
 ;;;X
@@ -25,10 +49,77 @@
 (defun goy ()
   (incf *mapy*)
   )
-	
+
+;;;Predicate - can you go up?
+(defun pcangoup ()
+  (< *mapy* *ymax*)
+  )
+
+;;;Predicate - can you go down?
+(defun pcangodown ()
+  (> *mapy* '0)
+  )
+
+;;;Predicate - can you go right?
+(defun pcangoright ()
+  (< *mapx* *xmax*)
+  )
+
+;;;Predicate - can you go left?
+(defun pcangoleft ()
+  (> *mapx* '0)
+  )
+
+;;;Where are you now?
+(defun wherenow ()
+  (coordtoloc *mapx* *mapy*)
+  )
+
+;;;Peeker for up in the map
+(defun whatup ()
+  (if (pcangoup)
+      (coordtoloc *mapx* (+ *mapy* 1))
+      (format t "Cannot go there."))
+  )
+
+;;;Peeker for down in the map
+(defun whatdown ()
+  (if (pcangodown)
+      (coordtoloc *mapx* (- *mapy* 1))
+      (format t "Cannot go there."))
+  )
+
+;;;Peeker for right in the map
+(defun whatright ()
+  (if (pcangoright)
+      (coordtoloc (+ *mapx* 1) *mapy*)
+      (format t "Cannot go there."))
+  )
+
+;;;Peeker for left in the map
+(defun whatleft ()
+  (if (pcangoleft)
+      (coordtoloc (- *mapx* 1) *mapy*)
+      (format t "Cannot go there."))
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;Creators, getters, setters;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;;;Creating a character (list)
 (defun newcharacter (name class)
   (list name class '100 '0 (newinventory))
+  )
+
+(defun makechar ()
+  (let ((name nil) (class nil))
+    (format t "Enter name:")
+    (setf name (read))
+    (format t "Enter class:")
+    (setf class (read))
+    (newcharacter name class)
+    )
   )
 
 ;;;Getter for character name
@@ -109,14 +200,15 @@
   (healthbar character)
   )
 
-;;;Game main function
+;;;Game Main function
 (defun game (character)
+  (createitems 7)
   (let ((input 1))
     (loop while (> input '0) do
 	 (present character)
 	 (terpri)
          (terpri)
-	 (format t "Location: (~D,~D)" *mapx* *mapy*)
+	 (format t "Location: ~A" (wherenow))
 	 (terpri)
 	 (format t "Enter number:")
 	 (setf input (read))
@@ -124,5 +216,3 @@
     
     )
   )
-
-
