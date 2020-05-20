e0d6e8f081b166548eb383d913c76ee8bb3ea0cc
mrdisturbing <juusoantti@gmail.com>
2020-05-20 14:40:42 +0300

    Visual improvements.

--- rpg.lisp
+++ rpg.lisp
@@ -10,6 +10,9 @@
 (defvar *xmax* 10)
 (defvar *ymax* 10)
 
+;;;Input error flag
+(defvar *error* 0)
+
 ;;:List for items in the map
 (defvar *itemlist* '())
 (setf *itemlist* nil)
@@ -21,6 +24,16 @@
     (setf *itemlist*
 	  (push (list (random 10) (random 10)) *itemlist*))
     )
+  )
+
+;;;Is there an item in current location?
+(defun pitemhere ()
+  (member (list *mapx* *mapy*) *itemlist* :test 'equal)
+  )
+
+;;;Symbol to string
+(defun lengthreturn (value)
+  (length (string value))
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -57,28 +70,28 @@
 (defun goright ()
   (if (pcangoright)
       (incf *mapx*)
-      (print "Error"))
+      (setf *error* 1))
   )
 
 ;;;Go left
 (defun goleft ()
   (if (pcangoleft)
       (decf *mapx*)
-      (print "Error"))
+      (setf *error* 1))
   )
 
 ;;;Go up
 (defun goup ()
   (if (pcangoup)
       (incf *mapy*)
-      (print "Error"))
+      (setf *error* 1))
   )
 
 ;;;Go down
 (defun godown ()
   (if (pcangodown)
       (decf *mapy*)
-      (print "Error"))
+      (setf *error* 1))
   )
 
 ;;;Predicate - can you go up?
@@ -110,46 +123,67 @@
 (defun whatup ()
   (if (pcangoup)
       (coordtoloc *mapx* (+ *mapy* 1))
-      (format t "Cannot go there."))
+      (return-from whatup ""))
   )
 
 ;;;Peeker for down in the map
 (defun whatdown ()
   (if (pcangodown)
       (coordtoloc *mapx* (- *mapy* 1))
-      (format t "Cannot go there."))
+      (return-from whatdown ""))
   )
 
 ;;;Peeker for right in the map
 (defun whatright ()
   (if (pcangoright)
       (coordtoloc (+ *mapx* 1) *mapy*)
-      (format t "Cannot go there."))
+      (return-from whatright ""))
   )
 
 ;;;Peeker for left in the map
 (defun whatleft ()
   (if (pcangoleft)
       (coordtoloc (- *mapx* 1) *mapy*)
-      (format t "Cannot go there."))
+      (return-from whatleft ""))
   )
 
 ;;;Draw directions for current location
 (defun drawdirections ()
   (if (pcangoup)
-      (format t "      ^  ~A" (whatup))
-      (format t "           "))
+      (format t "                  ^  ~A" (whatup))
+      (format t "                       "))
   (terpri)
-  (if (pcangoleft)
-      (format t "        <  ~A" (whatleft))
-      (format t "             "))
 
+  ;"1"-line in map screen
+  (dotimes (n 18)
+    (format t " ")
+    )
+  (format t "1")
+
+  (terpri)
+
+  ;"left"-portion in map screen
+  
+  (if (pcangoleft) (format t "< ") (format t "  "))
+
+  (format t "~A" (whatleft))
+  
+  (dotimes (n (- 10 (lengthreturn (whatleft))))
+    (format t " ")
+    )
+  
+  
+  
+  (format t "2  @  3")
+  
   (if (pcangoright)
-      (format t "           >  ~A" (whatright))
-      (format t "                "))
+      (format t "    >  ~A" (whatright))
+      (format t "         "))
+  (terpri)
+  (format t "                  4")
   (terpri)
   (if (pcangodown)
-      (format t "     v  ~A" (whatdown))
+      (format t "                  v  ~A" (whatdown))
       (format t ""))
   )
 
@@ -210,6 +244,11 @@
   (setf (fourth character) (+ (fourth character) points))
   )
 
+;;;Reset XP points of a character
+(defun resetxp (character)
+  (setf (fourth character) '0)
+  )
+
 ;;;Decrease the health of a character
 (defun losehealth (character health)
   (setf (third character) (- (third character) health))
@@ -242,7 +281,9 @@
 	  (getname character) (getrank character)
 	  (getclass character) (getxp character))
   (terpri)
-  (format t "You are carrying: ~A" (getinventory character))
+  (if (not (getinventory character))
+      (format t "You are carrying: ~A" (getinventory character))
+      (format t "You are not carrying anything."))
   (terpri)
   (terpri)
   (format t "Health:")
@@ -254,28 +295,50 @@
 (defun game (character)
   (setf *mapx* 0)
   (setf *mapy* 0)
+  (resetxp character)
   (createitems 7)
+  
   (let ((input 1))
+    ;Game loop starts
     (loop while (> input '0) do
+	 (setf *error* 0)
+         (format t "**************************************")
+         (terpri)
+	 ;Show the status of the character
 	 (present character)
 	 (terpri)
          (terpri)
+	 ;Print current location
 	 (format t "Location: ~A" (wherenow))
 	 (terpri)
+	 (terpri)
+	 (if (pitemhere)
+	     (format t "There is an item here.")
+	     (format t "Nothing here..."))
+	 (terpri)
+	 ;Draw what is around you
+	 (drawdirections)
+	 (terpri)
 	 (format t
-		 "Enter number (1 up, 2 left, 3 right, 4 down:")
+		 "Enter number:")
+         (terpri)
 	 (setf input (read))
 
 	 (cond ((eql input 1) (goup))
 	       ((eql input 2) (goleft))
 	       ((eql input 3) (goright))
 	       ((eql input 4) (godown))
-	       (t (format t "Error."))) 
+	       ((eql input 0) (format t "Game over."))
+	       (t (setf *error* 1))) 
 
-	 (if (member input '(1 2 3 4))
+	 (if (and (eql *error* 0) (not (eql input 0)))
 	     (addxp character 1)
-	 )
-    
-    )
+	     )
+
+	 (terpri)
+	 
+	 ) ;Game loop ends
+
+    (format t "XP result: ~D" (getxp character))
     )
   )
